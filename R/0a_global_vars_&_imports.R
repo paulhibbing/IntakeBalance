f43c8880f856a64732c96357485f96c0511e2035
Paul R. Hibbing <paulhibbing@gmail.com>
2022-08-05 13:57:53 -0500

    Set up some dependencies and global variables

---
+++ R/0a_global_vars_&_imports.R
@@ -0,0 +1,12 @@
+# Global Variables --------------------------------------------------------
+
+  if(getRversion() >= "2.15.1") utils::globalVariables(c(
+    ".", "bedrest", "Choi_is_NonWear", "days", "is_NonWear",
+    "is_Sleep", "is_WakeWear", "is_weekend", "Timestamp", "time",
+    "Tracy_is_Sleep", "TS", "valid_status", "weekday", "weekend"
+  ))
+
+# Imports -----------------------------------------------------------------
+
+  #' @import magrittr
+  NULL
